4eb22be0baccb4bfae92ee0af9d7f90b8630127c
sabirabanu10-jpg <sabirabanu10@gmail.com>
2026-04-17 19:54:56 +0400

    homework

---
+++ Pattern9.java
@@ -0,0 +1,21 @@
+class Pattern9
+{
+	public static void main(String [] args)
+	{
+		char alp= 'A';
+		for (int i =1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{ 
+		   
+
+			  
+				System.out.print(alp+ " ");
+		alp++;
+	
+			}
+		
+			System.out.println( );
+		}
+	}
+}
